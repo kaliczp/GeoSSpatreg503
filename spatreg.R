d0c5631763e46919d26e3db090def44933f7aaa5
Péter Kalicz <kaliczp@gmail.com>
2017-05-03 10:14:46 +0200

    First two rows

--- spatreg.R
+++ spatreg.R
@@ -1,6 +1,8 @@
 ## Simple linear spatial regression
 ## Grid creation
-x.co <- seq(0,1,0.1)
-y.co <- seq(0,1,0.1)
+x.co <- rep(seq(0,1,0.1),2)
+y.co <- rep(seq(0,0.1,0.1),length = 22, each=11)
+plot(x.co,y.co, asp=T)
 
 surf.df <- data.frame(x.co,y.co)
+plot(surf.df, asp=TRUE)
