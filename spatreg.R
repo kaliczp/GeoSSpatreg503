2ce7b532bbf7c1604f85891163626ef8dfbc42fb
Péter Kalicz <kaliczp@gmail.com>
2017-05-03 10:41:36 +0200

    contour plot

--- spatreg.R
+++ spatreg.R
@@ -7,4 +7,9 @@ plot(x.co,y.co, asp=T)
 surf.df <- data.frame(x=x.co, y=y.co)
 plot(surf.df, asp=TRUE)
 
-surf.df$z <- surf.df$x + surf.df$y
+surf.df$z <- surf.df$x + surf.df$y + rnorm(121,sd=0.05)
+plot(surf.df) # scatter plot matrix
+
+## Contour plot
+surf.mat <- matrix(surf.df$z, nrow=11)
+contour(seq(0,1,0.1), seq(0,1,0.1), surf.mat)
