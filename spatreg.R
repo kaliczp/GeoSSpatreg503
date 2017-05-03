29fd38b87172edd7223d2906ebd09e0cc825e7ed
Péter Kalicz <kaliczp@gmail.com>
2017-05-03 10:28:22 +0200

    z data simpe

--- spatreg.R
+++ spatreg.R
@@ -4,5 +4,7 @@ x.co <- rep(seq(0,1,0.1),11)
 y.co <- rep(seq(0,1,0.1),length = 121, each=11)
 plot(x.co,y.co, asp=T)
 
-surf.df <- data.frame(x.co,y.co)
+surf.df <- data.frame(x=x.co, y=y.co)
 plot(surf.df, asp=TRUE)
+
+surf.df$z <- surf.df$x + surf.df$y
