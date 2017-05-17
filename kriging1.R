8785047e9df4908422e0a5e7a54470a0d2fc9be0
Péter Kalicz <kaliczp@gmail.com>
2017-05-17 10:11:05 +0200

    kriging with glm

--- kriging1.R
+++ kriging1.R
@@ -10,3 +10,10 @@ lines(d, expcov(d, 0.7))
 lines(d, gaucov(d, 1.0, 0.3), lty = 3)
 
 variogram(topo.ls, 25)
+
+### Kriging
+## generalised least squares with covariance model
+topo.kr <- surf.gls(2, expcov, topo, d=0.7)
+correlogram(topo.kr,25)
+variogram(topo.kr,25)
+
